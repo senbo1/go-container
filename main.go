390758041de8cb30262118a521e60721700841d8
senbo1 <harshbhadu86@gmail.com>
2024-07-31 09:34:12 +0530

    implemented namespaces

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -15,6 +16,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		runContainer()
+	case "child":
+		runContainerProcess()
 	default:
 		fmt.Println("Unknown command")
 		os.Exit(1)
@@ -22,14 +25,35 @@ func main() {
 }
 
 func runContainer() {
-	fmt.Printf("Executing command: %v\n", os.Args[2:])
+	fmt.Printf("Starting container process: %v\n", os.Args[2:])
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error starting container: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func runContainerProcess() {
+	fmt.Printf("Inside container: %v\n", os.Args[2:])
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	if err := syscall.Sethostname([]byte("my-container")); err != nil {
+		fmt.Printf("Error setting hostname: %v\n", err)
+		os.Exit(1)
+	}
+
 	if err := cmd.Run(); err != nil {
-		fmt.Printf("Error running command: %v\n", err)
+		fmt.Printf("Error running container command: %v\n", err)
 		os.Exit(1)
 	}
 }
